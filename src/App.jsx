8eb6a30e85ad75a0f6d7a09674931eae72b6b670
shalem king <shalem712@gmail.com>
2024-10-13 15:16:50 +0530

    changes for other files

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,5 @@ import Home from './components/Home'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>);
+    <Home/>);
 }
